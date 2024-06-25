51ebb7db2afb7ec86602f3a640de28dbe0a5b58e
Samarajya Shrestha <samarajyastha@gmail.com>
2024-06-25 20:59:23 +0545

    chore: add console.log

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 const auth = (req, res, next) => {
   const token = req.cookies["token"];
 
+  console.log(token);
+
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (error, data) => {
       if (error) {
